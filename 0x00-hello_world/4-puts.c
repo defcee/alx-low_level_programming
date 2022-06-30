40387b2cff5dd11964a3f47029b21da439ed4bf1
defcee <nnamdi.fsa.enugu@gmail.com>
2022-06-30 15:30:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,28 +3,6 @@
 
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-int main(void)
-	
-{
-	
-		puts("\"Programming is like building a multilingual puzzle");
-		
-			return (0);
-			
-}include <stdio.h>
-
-
-
-/**
 
  * main - Entry point
 
