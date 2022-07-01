daca8a8b4d3111bc7fede047e1f7382f66cae6b6
defcee <nnamdi.fsa.enugu@gmail.com>
2022-07-01 01:02:07 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
+#include <stdio.h>                                                                                    
 
-/**
+/                                                                                                     
 
- * Description: main - Prints out a string to stdout.
+* main - Entry point                                                                                  
 
- * Return: 0 if success.
+* Return: Always 0 (sucess)                                                                           
 
- */
+*/                                                                                                    
 
-int main(void)
+int main(void)                                                                                        
 
-{
+{                                                                                                     
 
-puts("\"Programming is like building a multilingual puzzle");
+        puts("\"Programming is like building a multilingual puzzle");                                 
 
-return (0);
+        return (0);                                                                                   
 
-}
+} 
