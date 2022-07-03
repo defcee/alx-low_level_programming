d458b0b2e90cbd62d344b025acee329ea97d618b
defcee <nnamdi.fsa.enugu@gmail.com>
2022-07-03 15:42:48 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,7 +6,9 @@
  *                                                                                                    
  *Return: Always 0 (Success)                                                                         
  *
- **/                                                                                                  int main(void)                                                                                        
+ **/
+
+int main(void)                                                                                        
 {                                                                                                     
 
 	        char low;
