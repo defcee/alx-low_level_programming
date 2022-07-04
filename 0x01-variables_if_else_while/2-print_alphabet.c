64322875948439522c7e415d68e3b3ce430c329d
defcee <nnamdi.fsa.enugu@gmail.com>
2022-07-03 21:44:20 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,14 @@
  *Return: Always 0 (Success)                                                                         
  *
  **/
-
 int main(void)                                                                                        
-{                                                                                                     
+{
+	char low;
 
-	        char low;
-                for (low = 'a'; low <= 'z'; low++)
-			putchar(low);
-			printf("\n");
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		printf("\n");
 
-		return (0);
-}
+	return (0);
+}	
+
