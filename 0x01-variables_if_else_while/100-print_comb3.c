2fa4f2f8952f8bcc12ccf0fd6dd1659fe7a04569
defcee <nnamdi.fsa.enugu@gmail.com>
2022-07-04 06:27:36 -0700

    10prints

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Prints the numbers from 00 to 99
+ * main - Entry point
  *
- * Return: Always (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int c, i;
+	int d;
 
-	c = i = '0';
-
-	for (c = '0'; c <= '9'; c++)
+	for (d = 0; d < 100; d++)
 	{
-		for (i = '0'; i <= '9'; i++)
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
+		if (d != 99)
 		{
-			putchar(c);
-			putchar(i);
-
-			if ((c != '9') || (c == '9' && i != '9'))
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
