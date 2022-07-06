fbf73dd2b5b504d80a6a7df371b3b76ea33f2f6e
defcee <nnamdi.fsa.enugu@gmail.com>
2022-07-06 07:43:07 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -11,14 +11,12 @@ int main(void)
 
 	r = _isalpha('H');
 	_putchar(r + '0');
+	r = _isalpha('o');
 	_putchar(r + '0');
-        r = _isalpha(108);
+	r = _isalpha(108);
 	_putchar(r + '0');
 	r = _isalpha(';');
 	_putchar(r + '0');
 	_putchar('\n');
 	return (0);
 }
-
-
-
