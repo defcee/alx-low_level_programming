c25b5048194a5527f714c1e0d5cdcf0d454f1902
defcee <nnamdi.fsa.enugu@gmail.com>
2022-07-06 13:04:41 -0700

    4isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,13 @@
 #include "main.h"
+
 /**
- *_isalpha - check the code for Holberton School students.
- *@c: letter being tested
- *Return: Always 0
- * 
+ * _isalpha - check for lower case letter
+ * @c : character to check
+ * Return:0 or 1
  */
+
 int _isalpha(int c)
 {
-if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-return (1);
-else
-return (0);
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+
 }
