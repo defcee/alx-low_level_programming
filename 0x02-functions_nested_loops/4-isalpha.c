65e7c0379e56431e6073cbdce3dfb2cf0022866d
defcee <nnamdi.fsa.enugu@gmail.com>
2022-07-06 03:40:58 -0700

    4isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,18 @@
 #include "main.h"
 /**
- * _isalpha - a function that checks for alphabetic character
- * @c: single letter input
- * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
+ * _isalpha - entry point
+ * @c: c is an integer
+ * Description: return 1 if a letter is lower or uppercase
+ * Return: 1 or 0
  */
 int _isalpha(int c)
 {
-	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
 		return (1);
-	else
+	}
+	else 
+	{
 		return (0);
+	}
 }
