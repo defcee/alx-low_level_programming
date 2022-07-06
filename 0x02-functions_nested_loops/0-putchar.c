38696a4c9435bb80b06839197fc711f5cae9c9af
defcee <nnamdi.fsa.enugu@gmail.com>
2022-07-06 00:47:38 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
- *main - Entry point
+ * main - Prints "_putchar" followed by a new line.
  *
- * Return: Always 0
- */
+ * Return: Always 0.
+ * */
 int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-char *sh = "main";
-
-while (*sh)
-{
-_putchar(*sh);
-sh++;
-}	
-return (0);
+	return (0);
 }
 
