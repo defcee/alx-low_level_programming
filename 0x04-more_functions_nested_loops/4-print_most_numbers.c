903d66b879aed87d26d435e0fa7700828e77306e
defcee <nnamdi.fsa.enugu@gmail.com>
2022-07-09 07:49:05 -0700

    4most

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,22 @@
 #include "main.h"
+
 /**
- * print_most_numbers -  the numbers, from 0 to 9 without 2 and 4
+ * print_most_numbers - prints 0 - 9 except 2, 3
+ * Description: prints
  * Return: void
  */
+
 void print_most_numbers(void)
 {
-int i = 48;
-while (i <= 57)
-{
-if (i != 50 && i != 52)
-{
-_putchar(i);
-}
-i++;
-}
-_putchar('\n');
+	int a = 0;
+
+	while (a <= 9)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar(a + '0');
+		}
+		a++;
+	}
+	putchar('\n');
 }
