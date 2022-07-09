0aef589a0223cd9338a834006ee0ba0e5bff6000
defcee <nnamdi.fsa.enugu@gmail.com>
2022-07-09 07:40:01 -0700

    4most

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,15 +5,14 @@
  */
 void print_most_numbers(void)
 {
-	int i = 48;
-
-	while (i <= 57)
-	{
-		if (i != 50 && i != 52)
-		{
-			_putchar(i);
-		}
-		i++;
-	}
-	_putchar('\n');
+int i = 48;
+while (i <= 57)
+{
+if (i != 50 && i != 52)
+{
+_putchar(i);
+}
+i++;
+}
+_putchar('\n');
 }
