7de1d5b063b4d57f069a8c3e399cc62f17d9feb1
defcee <nnamdi.fsa.enugu@gmail.com>
2022-07-12 07:54:36 -0700

    3puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _puts -main entry.
  * @str: first parameter
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stlib.h>
+#include <stdlib.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
