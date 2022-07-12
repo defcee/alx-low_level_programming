23c1233903809e53a9a143b08396edbd2c03063f
defcee <nnamdi.fsa.enugu@gmail.com>
2022-07-12 07:12:04 -0700

    printrev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - print reverse
+ *@s: input string
+ *
+ * Description:  prints a string, in reverse
+ */
+void print_rev(char *s)
+{
+int left, len, i;
+len = 0;
+left = 0;
+for (i = 0; s[i] != '\0'; ++i)
+{
+len = i;
+}
+for (; len >= left; len--)
+{
+_putchar(s[len]);
+}
+_putchar('\n');
+}
