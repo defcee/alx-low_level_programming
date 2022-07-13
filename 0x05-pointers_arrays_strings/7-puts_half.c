60f64b1541a644322432fe83ab1eace81137feb9
defcee <nnamdi.fsa.enugu@gmail.com>
2022-07-12 21:45:31 -0700

    7puts

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,36 +1,18 @@
 #include "main.h"
 /**
- * puts_half - print half
- * @str: input string
- *
- * Description: prints half of a string
- * Return: Always (0);
+ * puts_half - half of a string.
+ * @str: string to be printed.
  */
 void puts_half(char *str)
 {
-int i;
-int diff;
-while (str[i] != '\0')
-{
-i++;
-}
-if (i % 2 == 1)
-{
-diff = (i + 1) / 2;
-while (str[diff] != '\0')
-{
-_putchar(str[diff]);
-diff++;
-}
-}
+int i = 0, len = 0, j;
+while (str[i++])
+len++;
+if ((len % 2) == 0)
+j = len / 2;
 else
-{
-diff = (i / 2);
-while (str[diff] != '\0')
-{
-_putchar(str[diff]);
-diff++;
-}
-}
+j = (len + 1) / 2;
+for (i = j; i < len; i++)
+_putchar(str[i]);
 _putchar('\n');
 }
