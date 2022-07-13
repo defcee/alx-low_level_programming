08b1fb32b639f8be864c68eea2b3830efcbfade2
defcee <nnamdi.fsa.enugu@gmail.com>
2022-07-12 21:34:04 -0700

    mainh

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
+size_t _strlen(const char *str);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -13,5 +13,7 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+int _putchar(char c);
+
 
 #endif
