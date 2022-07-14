f54bc957a72b864090e6d0aadd3ff90229d3d0d1
defcee <nnamdi.fsa.enugu@gmail.com>
2022-07-14 15:50:06 -0700

    3strt

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,22 @@
 #include "main.h"
 /**
- *  _strcmp - compare strings
- *  @s1: first input char
- *  @s2: second input char
- *
- *  Description: the function compares two strings
- *  Return: char count
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0 if matching, and ns1 - ns2 if not matching
  */
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
 {
-int i, diff;
-for (i = 0; s1[i] != '\0'; ++i)
+int i;
+/**
+ * We have reached the end of S1 OR we have reached the end of S2
+ *
+ */
+for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 {
-if (s1[i] > s2[i])
+if (s1[i] != s2[i])
 {
-diff = s1[i] - s2[i];
-return (diff);
-}
-else if (s1[i] < s2[i])
-{
-diff = s1[i] - s2[i];
-return (diff);
+return (s1[i] - s2[i]);
 }
 }
 return (0);
