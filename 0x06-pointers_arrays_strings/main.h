5dff50c4664b1f532eabf81f310bc28972a66211
defcee <nnamdi.fsa.enugu@gmail.com>
2022-07-14 14:02:32 -0700

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdlib>
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -10,10 +9,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-char *leet(char *);
+char *leet(char *n);
 char *rot13(char *);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
 
 #endif
