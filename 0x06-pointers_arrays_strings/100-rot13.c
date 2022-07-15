1dfdbb3c6604632c60dd795d1c5d5e6d35eefdff
defcee <nnamdi.fsa.enugu@gmail.com>
2022-07-14 17:12:18 -0700

    100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,26 @@
 #include "main.h"
 /**
- * rot13 -  a   function that encodes a string using rot13.
- * @s: An input string to encode using rot13
- * Return: An encode string
+ * rot13 - encode to rot13
+ *
+ * @s:input
+ *
+ * Return:s
  */
 char *rot13(char *s)
 {
-int i = 0;
-while (s[i] != '\0')
+int i, j;
+char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0; s[i] != '\0'; i++)
 {
-while ((s[i] >= 'a' && s[i] <= 'z') ||
-(s[i] >= 'A' && s[i] <= 'Z'))
+for (j = 0; j < 52; j++)
 {
-if ((s[i] >= 'a' && s[i] <= 'm') ||
-(s[i] >= 'A' && s[i] <= 'M'))
-s[i] += 13;
-else
-s[i] -= 13;
-i++;
+if (s[i] == alpha[j])
+{
+s[i] = rot[j];
+break;
 }
-i++;
+}
 }
 return (s);
 }
